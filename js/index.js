8ce0e47ea3f2b9e8e3acf273ff155e629565eb86
Vinicius Rocha <viniciusr.rocha@outlook.com>
2019-12-02 10:41:19 -0300

    Replicando a validação e o cálculo do IMC para todos os pacientes

--- js/index.js
+++ js/index.js
@@ -3,32 +3,38 @@ titulo.textContent = "Aparecida Nutricionista";
 var title = document.querySelector(".title");
 title.textContent = "Aparecida Nutricionista";
 
-var paciente = document.querySelector("#primeiro-paciente");
+var pacientes = document.querySelectorAll(".paciente");
 
-var tdPeso = paciente.querySelector(".info-peso");
-var peso = tdPeso.textContent;
 
-var tdAltura = paciente.querySelector(".info-altura");
-var altura = tdAltura.textContent;
+for (var i = 0; i < pacientes.length; i++) {
+    
+    var paciente = pacientes[i];
 
-var tdImc = paciente.querySelector(".info-imc");
+    var tdPeso = paciente.querySelector(".info-peso");
+    var peso = tdPeso.textContent;
 
-var pesoEhValido = true;
-var alturaEhValida = true;
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
 
-if (peso <= 0 || peso >= 1000) {
-    console.log("Peso inválido");
-    pesoEhValido =  false;
-    tdImc.textContent = "Peso Inválido";
-}
+    var tdImc = paciente.querySelector(".info-imc");
 
-if(altura < 0 || altura >= 3.00) {
-    console.log("Altura inválida");
-    alturaEhValida = false; 
-    tdImc.textContent = "Altura Inválido";   
-}
+    var pesoEhValido = true;
+    var alturaEhValida = true;
 
-if(alturaEhValida && pesoEhValido) {
-    var imc = peso / (altura * altura);
-    tdImc.textContent = imc;
+    if (peso <= 0 || peso >= 1000) {
+        console.log("Peso inválido");
+        pesoEhValido = false;
+        tdImc.textContent = "Peso Inválido";
+    }
+
+    if (altura < 0 || altura >= 3.00) {
+        console.log("Altura inválida");
+        alturaEhValida = false;
+        tdImc.textContent = "Altura Inválido";
+    }
+
+    if (alturaEhValida && pesoEhValido) {
+        var imc = peso / (altura * altura);
+        tdImc.textContent = imc.toFixed(2);
+    }
 }
