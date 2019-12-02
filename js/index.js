43b57a58782ab8cbbf70bbc5aa15c84e02510723
Vinicius Rocha <viniciusr.rocha@outlook.com>
2019-12-02 11:02:21 -0300

    Estilos com JavaScript

--- js/index.js
+++ js/index.js
@@ -25,12 +25,14 @@ for (var i = 0; i < pacientes.length; i++) {
         console.log("Peso inválido");
         pesoEhValido = false;
         tdImc.textContent = "Peso Inválido";
+        paciente.classList.add("paciente-invalido");
     }
 
     if (altura < 0 || altura >= 3.00) {
         console.log("Altura inválida");
         alturaEhValida = false;
         tdImc.textContent = "Altura Inválido";
+        paciente.classList.add("paciente-invalido");
     }
 
     if (alturaEhValida && pesoEhValido) {
