18ebe6696b8532408b373cf714f10bafbd0d76ea
Vinicius Rocha <viniciusr.rocha@outlook.com>
2019-12-02 09:37:27 -0300

    Buscando dados do paciente

--- js/index.js
+++ js/index.js
@@ -1,5 +1,14 @@
 var titulo = document.querySelector(".titulo");
-console.log(titulo);
-console.log(titulo.textContent);
-
 titulo.textContent = "Aparecida Nutricionista";
+
+var paciente = document.querySelector("#primeiro-paciente");
+
+var tdPeso = paciente.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+
+var tdAltura = paciente.querySelector(".info-altura");
+var altura = tdAltura.textContent;
+
+var imc = peso / altura * altura;
+
+console.log(imc);
