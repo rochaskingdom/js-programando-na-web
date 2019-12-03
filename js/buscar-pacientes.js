b0c9ea630bd0ba95f42bb1ba7e85344b997177de
Vinicius Rocha <viniciusr.rocha@outlook.com>
2019-12-03 12:55:19 -0300

    Capturando erros de requisicao

--- js/buscar-pacientes.js
+++ js/buscar-pacientes.js
@@ -7,17 +7,29 @@ botaoAdicionar.addEventListener("click", function () {
     xhr.open("GET", "https://api-pacientes.herokuapp.com/pacientes");
 
     xhr.addEventListener("load", function () {
+        var erroAjax = document.querySelector("#erro-ajax");
+        if (xhr.status == 200) {
 
-        var resposta = xhr.responseText;        
-        
-        var pacientes = JSON.parse(resposta);
+            erroAjax.classList.add("invisivel");
+            var resposta = xhr.responseText;
+            var pacientes = JSON.parse(resposta);
 
-        pacientes.forEach(function(paciente) {
+            pacientes.forEach(function (paciente) {
 
-            adicionaPacienteNaTabela(paciente);
-        });
-        
-        
+                adicionaPacienteNaTabela(paciente);
+            });
+
+        } else {
+            console.log(xhr.status);
+            console.log(xhr.responseText);
+            
+            erroAjax.classList.remove("invisivel");
+            
+        }
+
+
+
+
     });
 
     xhr.send();
